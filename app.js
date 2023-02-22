da4ed676b9c0ab0dacaa4fd916e55aa87ed322f0
Aayushma30 <aayushmap03@gmail.com>
2023-02-21 20:26:10 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+//function 
+function sum(firstNumber, secondNumber) {
+   // console.log('I am running from function')
+   return firstNumber + secondNumber
+}
+const result = sum(10, 20)
+
+console.log(result)
+
+
+const output = sum(1234, 5678)
+console.log(output)
+
+console.log ( 'Running 10 times')
