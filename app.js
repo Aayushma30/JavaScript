614461566adfbd00d0e79269fa08eaa6b43d5738
Aayushma30 <aayushmap03@gmail.com>
2023-02-24 21:31:52 -0500

    introduction to variables

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
-//function 
-function sum(firstNumber, secondNumber) {
-   // console.log('I am running from function')
-   return firstNumber + secondNumber
+ //Creating variable
+ //var, let, const, 
+ // to create variable you need to use either 'let' or 'const'   Variable declaration list cannot be empty.
+
+
+//Create variable
+//storing string in variable: string
+const myName = 'Aayushma Pal'
+let myCountry = 'USA'
+myCountry = 'Nepal'
+
+console.log(myCountry)
+//storing number in variable: Number
+const myFavNum = 7
+//  myFavNum = 7
+console.log(myFavNum)
+console.log ('Type of myFavNum variable: ', typeof myFavNum)
+ 
+//storing multiple values in variable: Array
+const countries = ['Nepal', 'USA', 'India']
+console.log (countries[1])
+console.log ('Type of myfavNum variable: ', typeof countries)
+
+//storing multiple values (different type) in variable: Array
+const aboutMe = ['Aayushma Pal' , 26 , ['JavaScript', 'Python']]
+console.log(aboutMe)
+console.log(aboutMe[2][1])
+
+//storing multiple values in variable: object
+ 
+const aboutMeObj = {
+   fullName: "Aayushma Pal",
+   yearsExperience: 5,
+   favLanguage: ['JavaScript', 'Python']
 }
-const result = sum(10, 20)
+console.log(aboutMeObj.favLanguage[1])
 
-console.log(result)
+aboutMeObj.yearsExperience = 6
+console.log(aboutMeObj)
+aboutMeObj.country = 'Nepal'
+console.log(aboutMeObj)
 
-
-const output = sum(1234, 5678)
-console.log(output)
-
-console.log ( 'Running 10 times')
